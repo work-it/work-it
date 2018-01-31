4a4816fb94ade87d668ee42fcecc933fed658329
Charles Thomas <charles@charlesmthomas.com>
2018-01-30 23:38:36 -0500

    finished scheduler!

--- client/components/practice-schedule/schedule-session.js
+++ client/components/practice-schedule/schedule-session.js
@@ -1,17 +1,52 @@
 import React from 'react'
 import {connect} from 'react-redux'
 import {withRouter} from 'react-router-dom'
+import moment from 'moment'
 import './schedule-session.css';
+import {times} from './times';
+
+const renderDetails = (type, start, userOne, userTwo, testUser) => {
+  const startLocal = moment.utc(start, 'HH:mm').local().format('HH:mm');
+  const startLocalTwelveHour = times.filter(time => time.value === startLocal)[0].text
+
+  console.log(startLocalTwelveHour)
+
+  switch (type) {
+    case 'available':
+      return (
+        <div>
+          <h4>{userOne} is available</h4>
+          <h4>{startLocalTwelveHour}</h4>
+        </div>
+      )
+    case 'waiting':
+      return (
+        <div>
+          <h4>I am available</h4>
+          <h4>{startLocalTwelveHour}</h4>
+        </div>
+      )
+    case 'paired':
+      return (
+        <div>
+          <h4>Paired with {userOne === testUser.id ? userTwo : userOne}</h4>
+          <h4>{startLocalTwelveHour}</h4>
+        </div>
+      )
+    default:
+      break;
+  }
+}
 
 const ScheduleSession = (props) => {
-  const { id, userOne, userTwo, start, testUser } = props;
+  const { id, userOne, userTwo, start, testUser, date } = props;
   let type;
   let classFortype = 'schedule-session ';
 
   if (userOne === testUser.id && !userTwo) {
     type = 'waiting';
     classFortype += type;
-  } else if (userOne === testUser.id && userTwo) {
+  } else if (userOne === testUser.id || userTwo === testUser.id) {
     type = 'paired';
     classFortype += type;
   } else {
@@ -20,26 +55,15 @@ const ScheduleSession = (props) => {
   }
 
   return (
-    <div className={classFortype}>
-      {type === 'available' && <div>
-        <h4>{userOne} is available</h4>
-        <h4>{start}</h4>
-      </div>}
-      {type === 'waiting' && <div>
-        <h4>I am available</h4>
-        <h4>{start}</h4>
-      </div>}
-      {type === 'paired' && <div>
-        <h4>Paired with {userTwo}</h4>
-        <h4>{start}</h4>
-      </div>}
+    <div className={classFortype} onClick={() => props.handleClick(type, date, id )}>
+      {renderDetails(type, start, userOne, userTwo, testUser)}
     </div>
   )
 }
 
 const mapState = (state) => {
   return {
-    testUser: {id: 99999}
+    testUser: {id: 55555}
   }
 }
 
---
+++ secret.js
@@ -0,0 +1,22 @@
+module.exports = {
+  twilioClient: {
+      accountSid: 'AC72ecdf07b809798a2b893a1b5b47d017',
+      authToken: 'ba640c836a721fd876a5d7c8a2b80afd',
+      keySid:'SK8f787afeb5bd3c142ab8f2e54cd960a3',
+      keySecret:'YparFV01d6RKKKFXIwPnTR6IeyDAfgWL',
+      twilioPhone:'+15162070820'
+  },
+  facebook: {
+      token: 'c67d4693eaadd8241c289c1100d23656',
+      secret: 'fda3e7a149ec99436a16ddcee11e3c23',
+      appid: '1572631936117530',
+  },
+  google: {
+      clientId: '937265417722-7kkj46sjveg780va7s35e2d6nsab0vci.apps.googleusercontent.com',
+      secret: 'TgRooC3k2FQlwPl6xsQT3l1J'
+  },
+  firebase: {
+      webApiKey: 'AIzaSyANNWXO-f7_9AOw5QuFZruUIr4IinSs0og',
+
+  }
+}
